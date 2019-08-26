8df9bab33116f6ef879761e7098a1cbba72fbe52
Chris Muzzin <cmuzzin@uark.edu>
2019-08-25 22:16:02 -0700

    added in leadlet dependency and demo

--- front-end/src/app/tabs/tab3/tab3.page.ts
+++ front-end/src/app/tabs/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  map: Map;
 
   constructor() {}
+  ionViewDidEnter() {
+    this.leafletMap(); }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+
+    marker([28.6, 77]).addTo(this.map)
+      .bindPopup('Ionic 4 <br> Leaflet.')
+      .openPopup();
+  }
+
 
 }
