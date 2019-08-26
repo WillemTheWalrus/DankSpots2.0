0f20768dc04762f6728e451b7fe64c8c67e3ab67
Chris Muzzin <cmuzzin@uark.edu>
2019-08-25 22:07:20 -0700

    tabs module scaffold reformat

--- front-end/src/app/tabs/tabs.router.module.ts
+++ front-end/src/app/tabs/tabs.router.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+              import('./tab1/tab1.module').then(m => m.Tab1PageModule)
           }
         ]
       },
@@ -23,7 +23,7 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../map/map.module').then(m => m.MapPageModule)
+              import('./map/map.module').then(m => m.MapPageModule)
           }
         ]
       },
@@ -33,7 +33,7 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('./tab3/tab3.module').then(m => m.Tab3PageModule)
           }
         ]
       },
